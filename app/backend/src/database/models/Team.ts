349361033f5ee0b87bae5a1f08c939a3f2be8a6f
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-15 15:55:55 -0300

    feat: cria a model Team

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,25 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+export default Team;
